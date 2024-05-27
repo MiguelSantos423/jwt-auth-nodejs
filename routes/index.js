101cc473c248ea0a89434d64f064019cc17e015b
MiguelSantos423 <super.prior00@gmail.com>
2024-05-27 22:47:21 +0400

    minor update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
+require("dotenv").config()
 const Users = require("../models/users");
 const jwt = require('jsonwebtoken')
-require("dotenv").config()
 const secret = process.env.JWT_SECRET
 const router = require("express").Router();
 
